6aa04d6e11e868151869403d8570b50b86fde115
unknown <as810as98@gmail.com>
2020-10-14 16:22:06 +0800

    first upload

---
+++ convert.py
@@ -0,0 +1,33 @@
+import re
+import os
+#將想刪除字幕ID與加上line:77%的srt檔
+#存放至convert.py的檔案夾中
+#隨後開啟demo.py，只需輸入srt檔的名稱即可
+def convert_TimeNuber(filename): #filename為srt的檔案名稱
+    with open(filename,'r+',encoding="utf-8") as f:
+        texts = f.readline()
+        rename_time=[]
+        # for text in texts:
+        while texts:
+            time=re.compile(r'(\d\d):(\d\d):(\d\d),(\d{3}) --> (\d\d):(\d\d):(\d\d),(\d{3})$')
+            times=re.sub(time,r'\1:\2:\3.\4 --> \5:\6:\7.\8 line:77%',str(texts))
+            texts = f.readline()
+            rename_time.append(times)
+    with open('output.srt','w',encoding="utf-8") as n:
+        n.writelines(rename_time)
+        print(rename_time)
+    with open('output.srt','r',encoding="utf-8") as f:
+        texts = f.readline()
+        delete_number=[]
+        while texts:
+        # for text in texts:
+            number=re.compile(r'^\d+\s$')
+            numbers=re.sub(number,"",texts)
+            texts = f.readline()
+            delete_number.append(numbers)
+            filename=filename.replace('.srt','.vtt')
+    with open('convert'+filename,'w',encoding="utf-8") as n:
+        n.writelines('WEBVTT\nKind:captions\nLanguage:zh-TW\n')
+        n.writelines(delete_number)
+        print(delete_number)
+    os.remove('output.srt')
---
+++ demo.py
@@ -0,0 +1,3 @@
+from convert import convert_TimeNuber
+
+convert_TimeNuber('filename.srt') #括弧內輸入想轉檔的srt檔
